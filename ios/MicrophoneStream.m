a12382b1bae92d8a6620618f21627355ac2e564f
Chad Smith <chad@nospam.me>
2019-05-20 11:03:33 -0500

    record in pcm & convert to ulaw before sending

--- ios/MicrophoneStream.m
+++ ios/MicrophoneStream.m
@@ -1,8 +1,13 @@
 #import "MicrophoneStream.h"
 
+#define BIAS (0x84)
+#define CLIP 8159
+
 @implementation MicrophoneStream {
     AudioQueueRef _queue;
     AudioQueueBufferRef _buffer;
+    AVAudioSessionCategory _category;
+    AVAudioSessionMode _mode;
 }
 
 void inputCallback(
@@ -15,9 +20,50 @@ void inputCallback(
     [(__bridge MicrophoneStream *) inUserData processInputBuffer:inBuffer queue:inAQ];
 }
 
+int seg_uend[8] = { 0x3F, 0x7F, 0xFF, 0x1FF, 0x3FF, 0x7FF, 0xFFF, 0x1FFF };
+
+static int search(int val, int *table, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (val <= *table++)
+            return (i);
+    }
+    return (size);
+}
+
+int linear2ulaw(int pcm_val) {
+    int mask;
+    int seg;
+    int uval;
+
+    pcm_val = pcm_val >> 2;
+    if (pcm_val < 0) {
+        pcm_val = -pcm_val;
+        mask = 0x7F;
+    } else {
+        mask = 0xFF;
+    }
+    if (pcm_val > CLIP)
+        pcm_val = CLIP;
+    pcm_val += (BIAS >> 2);
+
+    seg = search(pcm_val, seg_uend, 8);
+
+    if (seg >= 8)
+        return (0x7F ^ mask);
+    else {
+        uval = (seg << 4) | ((pcm_val >> (seg + 1)) & 0xF);
+        return (uval ^ mask);
+    }
+}
+
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(init:(NSDictionary *) options) {
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    _category = [session category];
+    _mode = [session mode];
+
     UInt32 bufferSize = options[@"bufferSize"] == nil ? 8192 : [options[@"bufferSize"] unsignedIntegerValue];
 
     AudioStreamBasicDescription description;
@@ -28,8 +74,8 @@ RCT_EXPORT_METHOD(init:(NSDictionary *) options) {
     description.mFramesPerPacket = options[@"framesPerPacket"] == nil ? 1 : [options[@"framesPerPacket"] unsignedIntegerValue];
     description.mBytesPerFrame = options[@"bytesPerFrame"] == nil ? 2 : [options[@"bytesPerFrame"] unsignedIntegerValue];
     description.mBytesPerPacket = options[@"bytesPerPacket"] == nil ? 2 : [options[@"bytesPerPacket"] unsignedIntegerValue];
-    description.mFormatID = kAudioFormatULaw;
-    description.mFormatFlags = 0;
+    description.mFormatID = kAudioFormatLinearPCM;
+    description.mFormatFlags = kAudioFormatFlagIsSignedInteger;
 
     AudioQueueNewInput(&description, inputCallback, (__bridge void *) self, NULL, NULL, 0, &_queue);
     AudioQueueAllocateBuffer(_queue, (UInt32) (bufferSize * 2), &_buffer);
@@ -48,25 +94,30 @@ RCT_EXPORT_METHOD(start) {
 RCT_EXPORT_METHOD(pause) {
     AudioQueuePause(_queue);
     AudioQueueFlush(_queue);
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:_category
+                   error:nil];
+    [session setMode:_mode
+               error:nil];
 }
 
 RCT_EXPORT_METHOD(stop) {
     AVAudioSession *session = [AVAudioSession sharedInstance];
-    [session setCategory:AVAudioSessionCategoryPlayback
+    [session setCategory:_category
                    error:nil];
+    [session setMode:_mode
+               error:nil];
     AudioQueueStop(_queue, YES);
 }
 
 - (void)processInputBuffer:(AudioQueueBufferRef)inBuffer queue:(AudioQueueRef)queue {
-    NSData* audioData = [NSData dataWithBytes:inBuffer->mAudioData
-                                       length:inBuffer->mAudioDataByteSize];
+    SInt16 *audioData = inBuffer->mAudioData;
+    UInt32 count = inBuffer->mAudioDataByteSize / sizeof(SInt16);
 
-    const unsigned char *dataBuffer = (const unsigned char *)[audioData bytes];
-    NSUInteger dataLength  = [audioData length];
-    NSMutableArray *array  = [NSMutableArray arrayWithCapacity:dataLength];
+    NSMutableArray *array  = [NSMutableArray arrayWithCapacity:count];
 
-    for (int i = 0; i < dataLength; ++i)
-     [array addObject:[NSNumber numberWithInteger:dataBuffer[i]]];
+    for (int i = 0; i < count; ++i)
+        [array addObject:[NSNumber numberWithInteger:linear2ulaw(audioData[i])]];
 
     [self sendEventWithName:@"audioData" body:array];
     AudioQueueEnqueueBuffer(queue, inBuffer, 0, NULL);
